adc28671a60214b4afb04ed24e9a8d61985e7819
koharu <le2.lsb108@gmail.com>
2024-09-25 19:13:19 +0900

    リレーションの設定

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Prophecy\Doubler\Generator\Node\ReturnTypeNode;
 
 class Todo extends Model
 {
     use HasFactory;
     protected $fillable = [   // fillable＝書き換え可能  guarded=書き換え不可
+        'category_id',
         'content'
     ];
+    // public function todos()
+    // {
+    //     return $this->hasMany('App\Models\Todo');
+    // }
+    // public function todos()
+    // {
+    //     return $this->belongsTo('App\Models\Todo');
+    // }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
